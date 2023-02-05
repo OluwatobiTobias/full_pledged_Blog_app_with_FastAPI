6cf13c0cd711ef4e10532cc7676a494af36c188e
OLuwatobiTobias <giwaoluwatobi@gmail.com>
2023-02-05 08:08:42 +0100

    ...

--- blog_app/main.py
+++ blog_app/main.py
@@ -12,11 +12,13 @@ def home_page() -> Dict[str, str]:
 def bye_page() -> Dict[str, str]:
     return {'bye_page': 'BYE!!!'}
 
+def start_application():
+    # if __name__ == "__main__":
+        from sys import argv
 
-if __name__ == "__main__":
-
-    from sys import argv
+        print(argv)
+        port if (port := int(argv[1])) else (port := 8000)
+        uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
     
-    print(argv)
-    port if (port := int(argv[1])) else (port := 8000)
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    # else:
+    #     print('is not running file directly')
